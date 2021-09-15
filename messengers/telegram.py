67095393bad1782078e54fe6148a8c9160481fe4
Guilherme H <ghantani@gmail.com>
2021-09-15 16:21:52 -0300

    feat: Add Echo to the Bot

--- messengers/telegram.py
+++ messengers/telegram.py
@@ -7,7 +7,7 @@ class TelegramBot:
         self.token = open('token_telegram.txt').read()
         self.url_base = f'https://api.telegram.org/bot{self.token}/'
     
-    #Iniciar o bot
+    #Start Bot
     def startBot(self):
       update_id = None
       while True:
@@ -17,20 +17,29 @@ class TelegramBot:
           for item in data:
             update_id = item['update_id']
             chat_id = item["message"]["from"]["id"]
-            answer = self.createAnswer()
+            is_first_message = item["message"]["message_id"] == 1
+            answer = self.createAnswer(is_first_message, item)
             self.reply(answer, chat_id)
 
-    #Obter mensagens
+    #Get messages from chat
     def getMessages(self, update_id):
       link_request = f'{self.url_base}getUpdates?timeout=20'
       if update_id:
         link_request = f'{link_request}&offset={update_id + 1}'
       message = requests.get(link_request)
       return json.loads(message.content)
-    #Criar Respostas 
-    def createAnswer(self):
-      return 'Só quero que me diga, qual o papo? '
-    #responder mensagens 
+
+    #Create Answer
+    def createAnswer(self, is_first_message, item):
+      item = item['message']['text']
+      if is_first_message == True:
+        return "Olá, seja bem-vindo ao MedBot, mande uma mensagem, e eu repetirei ela!"
+
+      else:
+        return item
+
+
+    #Reply messages
     def reply(self, answer, chat_id):
       replyLink = f'{self.url_base}sendMessage?chat_id={chat_id}&text={answer}'
       requests.get(replyLink)
