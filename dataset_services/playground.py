1c606d3146b023e8a0ae0022f88b52a6ccbb49c8
Iuryglabson <iuryglabson18@gmail.com>
2021-09-22 17:38:14 -0300

    Feat: Cleaned dataset.

--- dataset_services/playground.py
+++ dataset_services/playground.py
@@ -1,7 +1,25 @@
 from dataset_services import PandasDataset
-from dataset_services import transformDataset
+from unicodedata import normalize
 
-pandasDataset = PandasDataset('datasets/dataset.csv')
-transformDataset(pandasDataset.dataset)
+
 pandasDataset = PandasDataset('datasets/modifiedDataset.csv')
 # pandasDataset.saveDataset()
+'''
+file = open('datasets/symptons.txt')
+for i in file:
+    pandasDataset.removeColumnByName(i[0:-1])
+'''
+
+'''
+file_portuguese = open('datasets/symptons_portuguese.txt', 'r')
+file_english = open('datasets/symptons_english.txt', 'r')
+portuguese = []
+english = []
+for i in file_portuguese:
+    portuguese.append(normalize('NFKD', i[0:-1]).encode('ASCII','ignore').decode('ASCII'))
+for i in file_english:
+    english.append(normalize('NFKD', i[0:-1]).encode('ASCII','ignore').decode('ASCII'))
+
+for i in range(len(portuguese)):
+    pandasDataset.renameColumnElement(english[i], portuguese[i])
+'''
