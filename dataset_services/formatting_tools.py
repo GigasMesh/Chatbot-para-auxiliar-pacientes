f4f7990ba777b12702952a6dbe5a919a82992141
Iuryglabson <iuryglabson18@gmail.com>
2021-09-22 15:25:04 -0300

    Fix: use first row in transformation.

--- dataset_services/formatting_tools.py
+++ dataset_services/formatting_tools.py
@@ -11,16 +11,16 @@ def transformDataset(dataset):
                 symptons.append(dataset[dataset.columns[column]][row])
 
     symptons.pop(4)
-    zeroData = np.zeros(shape=(len(dataset) - 1, len(symptons)), dtype='int32')
+    zeroData = np.zeros(shape=(len(dataset), len(symptons)), dtype='int32')
     dataframe = pd.DataFrame(zeroData, columns=symptons)
 
-    for row in range(1, len(dataset)):
+    for row in range(0, len(dataset)):
         for column in range(0, len(dataset.columns)):
             if not pd.isna(dataset[dataset.columns[column]][row]):
                 if column == 0:
-                    dataframe.loc[row - 1, 'disease'] = dataset[dataset.columns[column]][row]
+                    dataframe.loc[row, 'disease'] = dataset[dataset.columns[column]][row]
                 else:
-                    dataframe.loc[row - 1, dataset[dataset.columns[column]][row]] = 1
+                    dataframe.loc[row, dataset[dataset.columns[column]][row]] = 1
 
     dataframe.to_csv('datasets/modifiedDataset.csv', index=False)
 
--- dataset_services/playground.py
+++ dataset_services/playground.py
@@ -1,5 +1,7 @@
 from dataset_services import PandasDataset
+from dataset_services import transformDataset
 
-
+pandasDataset = PandasDataset('datasets/dataset.csv')
+transformDataset(pandasDataset.dataset)
 pandasDataset = PandasDataset('datasets/modifiedDataset.csv')
 # pandasDataset.saveDataset()
