05c038518965274d3a8dba60212189b63daf56ab
Iuryglabson <iuryglabson18@gmail.com>
2021-09-28 13:50:46 -0300

    Fix: Use first_name instead of username.

--- mainTelegram.py
+++ mainTelegram.py
@@ -12,22 +12,22 @@ def message_received(message):
     userId = message.from_user.id
     if userId not in botObject.usersSymptons:
         bot.send_message(chat_id=message.from_user.id, text=botObject.returnQuestion(userId, message))
-        print("informações do usuário %s: " % message.from_user.username, botObject.usersInformations[userId])
+        print("informações do usuário %s: " % message.from_user.first_name, botObject.usersInformations[userId])
 
     # Symptons
     else:
         result = botObject.returnMenu(userId, message)
         if result:
-            print("Sintomas do usuário %s: " % message.from_user.username, botObject.usersSymptons[userId])
+            print("Sintomas do usuário %s: " % message.from_user.first_name, botObject.usersSymptons[userId])
             bot.send_message(chat_id=message.from_user.id, text=result)
         else:
             decisionTree = DecisionTree('dataset_services/datasets/modifiedDataset.csv')
             symptons = botObject.usersSymptons[userId]
             disease = decisionTree.returnDisease(symptons)[0]
-            print("Doença do usuário %s: " % message.from_user.username, disease)
+            print("Doença do usuário %s: " % message.from_user.first_name, disease)
             bot.send_message(chat_id=message.from_user.id, text="Não há mais sintomas para serem mostrados")
             bot.send_message(chat_id=message.from_user.id, text="Sua doença pode ser: %s" % disease)
-            print("Sintomas do usuário %s: " % message.from_user.username, botObject.usersSymptons[userId])
+            print("Sintomas do usuário %s: " % message.from_user.first_name, botObject.usersSymptons[userId])
             botObject.resetUserInformations(userId)
 
 
