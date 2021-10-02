b308eb82f765017623aba71ca508378482db7644
Guilherme H <ghantani@gmail.com>
2021-10-02 15:58:56 -0300

    Feat: getNearHospital

---
+++ services/getHospital.py
@@ -0,0 +1,32 @@
+import pycep_correios
+from geopy.geocoders import Nominatim
+from googleplaces import GooglePlaces, types, lang
+import requests
+import json
+
+def getNearHospital(cep):
+    endereco = pycep_correios.get_address_from_cep(cep)
+
+    geolocator = Nominatim(user_agent="teste123")
+    location = geolocator.geocode(endereco['logradouro'] + ", " + endereco['cidade'] + " - " + endereco['bairro'])
+
+
+    API_KEY = open('services/apiKeyGoogle.txt').read()
+
+    google_places = GooglePlaces(API_KEY)
+
+    query_result = google_places.nearby_search(
+        lat_lng={'lat': location.latitude, 'lng': location.longitude},
+        radius=5000,
+        types=[types.TYPE_HOSPITAL])
+
+    if query_result.has_attributions:
+        print(query_result.html_attributions)
+
+    for place in query_result.places:
+        if 'UBS' in place.name or 'Hospital' in place.name or 'Unidade de Saúde' in place.name or 'Unidade de Saude' in place.name:
+            return place
+
+    return None
+
+print(getNearHospital('66110350').name)
