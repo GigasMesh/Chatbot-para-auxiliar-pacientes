32689dba9301b2b4e5d9bf08a77dfc9aeff0641a
Giovanne Souza <giovanne_cesar@outlook.com>
2021-10-04 19:53:56 -0300

    Fix: Ranking hospitals by distance

--- services/getHospital.py
+++ services/getHospital.py
@@ -1,30 +1,30 @@
 import pycep_correios
-from geopy.geocoders import Nominatim
+from services.getLocation import getLocationByCEP
 from googleplaces import GooglePlaces, types, lang
-import requests
-import json
 
 def GetNearHospital(cep):
-    endereco = pycep_correios.get_address_from_cep(cep)
-    print(endereco)
-    geolocator = Nominatim(user_agent="teste123")
-    location = geolocator.geocode(endereco['logradouro'] + ", " + endereco['cidade'] + " - " + endereco['bairro'])
-    if not location:
-        return None
 
     API_KEY = open('services/apiKeyGoogle.txt').read()
+
+    endereco = pycep_correios.get_address_from_cep(cep)
+    print(endereco)
+    location = getLocationByCEP(API_KEY, endereco['cep'])
+    if not location:
+        return None
 
     google_places = GooglePlaces(API_KEY)
 
     query_result = google_places.nearby_search(
-        lat_lng={'lat': location.latitude, 'lng': location.longitude},
+        lat_lng={'lat': location['lat'], 'lng': location['lng']},
         radius=5000,
-        types=[types.TYPE_HOSPITAL])
+        types=[types.TYPE_HOSPITAL],
+        rankby='distance')
 
-
+    print('Locais encontrados:')
     if query_result.has_attributions:
         print(query_result.html_attributions)
     for place in query_result.places:
+        print(place.name)
         if 'UBS' in place.name or 'Hospital' in place.name or 'Unidade de Saúde' in place.name or 'Unidade de Saude' in place.name:
             return place
 
